1811397d29b2df3b1f2ed54f464cf2539f199f79
ci-bb-user <ci-user@globetracker.com>
2023-10-10 22:36:54 +0100

    feat:  added scraper for silo

---
+++ scrapers/silo.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+Date.prototype.getWeek = function(): number {
+    const date = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    const dayNum = date.getUTCDay() || 7;
+
+    date.setUTCDate(date.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+
+    return Math.ceil((((date.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+};
+
+export const scrapeMenu = async (): Promise<string | null> => {
+    try {
+        const { data } = await axios.get('https://silo.fo/matskra/');
+        const $ = cheerio.load(data);
+
+        const currentWeek = new Date().getWeek();
+        const dayOfWeek = new Date().getDay() - 1;
+
+        if (dayOfWeek === 5 || dayOfWeek === 6) {
+            return "Ongin matskrá í vikuskiftinum";
+        }
+
+
+        let targetContent: string | null = null;
+
+        $('.flex_cell').each((index, weekElement) => {
+            const weekTitle = $(weekElement).find('h3.ItemTitle b').text().trim();
+
+            if (weekTitle.includes(`Vika ${currentWeek}`)) {
+                const menuContent = $(weekElement).find(`#cphContent_rGroups_rItems_0_lTitleAddtional_${dayOfWeek}`).text().trim();
+
+                if (menuContent) {
+                    targetContent = menuContent;
+                    return false;
+                }
+            }
+        });
+
+        return targetContent;
+
+    } catch (error) {
+        console.error(`Error occurred: ${error}`);
+        return null;
+    }
+};
