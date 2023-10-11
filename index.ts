03e1e47efeb7a2ded026d7b4bf31892b54443a51
Torkil Djurhuus <torkil.djurhuus@globetracker.com>
2023-10-12 00:26:49 +0100

    * feat: added ocr for fk's menu

--- index.ts
+++ index.ts
@@ -1,32 +1,12 @@
-import { App } from '@slack/web-api';
-import {scrapeSiloMenu} from "./scrapers/silo";
-import {scrapeFkMenu} from "./scrapers/fk";
+import { scrapeSiloMenu } from './scrapers/silo';
 import {scrapeMiklagardurMenu} from "./scrapers/miklagardur";
+import {scrapeFkMenu} from "./scrapers/fk";
 
-const slackToken = 'your-slack-token-here'; // replace with your actual Slack bot token
-
-const app = new App({
-    token: slackToken,
-});
-
-const runScrapers = async () => {
-    try {
-        const [imageTextResult, website1Result, website2Result] = await Promise.all([
-            scrapeSiloMenu(),
-            scrapeFkMenu(),
-            scrapeMiklagardurMenu(),
-        ]);
-
-        const finalMessage = [imageTextResult, website1Result, website2Result].join('\n\n');
-
-        // Post to Slack
-        await app.chat.postMessage({
-            channel: 'channel-id', // replace with your Slack channel id
-            text: finalMessage,
-        });
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-runScrapers();
+async function main() {
+    const silo = await scrapeSiloMenu();
+    console.log('Silo: ',silo);
+    const miklagardur = await scrapeMiklagardurMenu();
+    console.log('Miklagarður: ',miklagardur);
+    const fk = await scrapeFkMenu();
+    console.log('FK: ',fk);
+}
