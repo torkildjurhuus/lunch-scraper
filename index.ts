8457e842d76c416eee577b6db61c66d6d47a98ec
Torkil Djurhuus <torkil.djurhuus@globetracker.com>
2023-10-11 20:51:30 +0100

    feat: added ocr for fk's menu (#7)
    Co-authored-by: ci-bb-user <ci-user@globetracker.com>

--- index.ts
+++ index.ts
@@ -1,8 +1,32 @@
-import { scrapeSiloMenu } from './scrapers/silo';
+import { App } from '@slack/web-api';
+import {scrapeSiloMenu} from "./scrapers/silo";
+import {scrapeFkMenu} from "./scrapers/fk";
+import {scrapeMiklagardurMenu} from "./scrapers/miklagardur";
 
-async function main() {
-    const foodTitles = await scrapeSiloMenu();
-    console.log(foodTitles);
-}
+const slackToken = 'your-slack-token-here'; // replace with your actual Slack bot token
 
-main().catch((error) => console.error(`An error occurred: ${error}`));
+const app = new App({
+    token: slackToken,
+});
+
+const runScrapers = async () => {
+    try {
+        const [imageTextResult, website1Result, website2Result] = await Promise.all([
+            scrapeSiloMenu(),
+            scrapeFkMenu(),
+            scrapeMiklagardurMenu(),
+        ]);
+
+        const finalMessage = [imageTextResult, website1Result, website2Result].join('\n\n');
+
+        // Post to Slack
+        await app.chat.postMessage({
+            channel: 'channel-id', // replace with your Slack channel id
+            text: finalMessage,
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+runScrapers();
