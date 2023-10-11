ff32e311a0bc68b4a26e2b35e19b40f5c6bd8dfd
Torkil Djurhuus <torkil.djurhuus@globetracker.com>
2023-10-12 00:38:48 +0100

    feat: add netlify functions

---
+++ functions/scrapers.js
@@ -0,0 +1,38 @@
+import { scrapeSiloMenu } from '../scrapers/silo';
+import { scrapeMiklagardurMenu } from '../scrapers/miklagardur';
+import { scrapeFkMenu } from '../scrapers/fk';
+
+exports.handler = async (event) => {
+    try {
+        let scraperType = event.queryStringParameters.scraper;
+
+        let result;
+        switch (scraperType) {
+            case 'silo':
+                result = await scrapeSiloMenu();
+                break;
+            case 'miklagardur':
+                result = await scrapeMiklagardurMenu();
+                break;
+            case 'fk':
+                result = await scrapeFkMenu();
+                break;
+            default:
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ error: 'Invalid scraper type provided' }),
+                };
+        }
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ menu: result }),
+        };
+    } catch (error) {
+        console.error(`Error occurred: ${error}`);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Internal Server Error' }),
+        };
+    }
+};
